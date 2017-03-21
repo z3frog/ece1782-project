e2ab6af1ca3d1ebdce7c4edb3f47ad0490b2a2a5
Andre Lo <andre.lo@outlook.com>
2017-03-21 00:16:10 -0400

    check in hough CPU
    -replaced the compile.sh with a better Makefile
    -CPU code is compiled with CUDA compiler so that we can copy the
    makefile and main funcs when we implement CUDA version

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#To compile, just execute "compile.sh <filename without .cu extension>
-nvcc $1.cu -o $1 -arch sm_52
---
+++ hough_cpu/readme.txt
@@ -0,0 +1,8 @@
+To compile:
+make
+
+To clean:
+make clean
+
+To run:
+./hough_cpu Pentagon.png transformed.png
